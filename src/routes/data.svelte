cb166b762aae0d02035622f9beea385cfa85eeec
Frankie Mercado <oeluer@gmail.com>
2022-02-13 20:54:07 -0800

    create new events

--- src/routes/data.svelte
+++ src/routes/data.svelte
@@ -11,25 +11,25 @@
 	let usHolidays = holidaysForYear;
 	let birthdays = [
 		{
-			date: '1960-01-02 00:00:00',
+			date: '1960-01-02T00:00:00Z',
 			type: 'Birthday',
 			name: 'Dad',
 			image: 'https://picsum.photos/100/100',
 		},
 		{
-			date: '1961-03-04 00:00:00',
+			date: '1961-03-04T00:00:00Z',
 			type: 'Birthday',
 			name: 'Mom',
 			image: 'https://picsum.photos/100/100',
 		},
 		{
-			date: '1962-05-06 00:00:00',
+			date: '1962-05-06T00:00:00Z',
 			type: 'Birthday',
 			name: 'Zoey',
 			image: 'https://picsum.photos/100/100',
 		},
 		{
-			date: '1963-07-08 00:00:00',
+			date: '1963-07-08T00:00:00Z',
 			type: 'Birthday',
 			name: 'Frankie',
 			image: 'https://picsum.photos/100/100',
@@ -37,13 +37,13 @@
 	];
 	let anniversaries = [
 		{
-			date: '1961-09-10 00:00:00',
+			date: '1961-09-10T00:00:00Z',
 			type: 'Anniversary',
 			name: 'Mom & Dad',
 			image: 'https://picsum.photos/100/100',
 		},
 		{
-			date: '1910-11-12 00:00:00',
+			date: '1910-11-12T00:00:00Z',
 			type: 'Anniversary',
 			name: 'Aunt & Uncle',
 			image: 'https://picsum.photos/100/100',
@@ -55,14 +55,14 @@
 		id: window.crypto.randomUUID(),
 	}));
 
-	database.set(events);
-
 	// compare in 2022 which is a leap year
 	events.sort((a, b) => {
 		const aDate = a.date.replace(/^(\d{4})/, '2022');
 		const bDate = b.date.replace(/^(\d{4})/, '2022');
 		return new Date(aDate) - new Date(bDate);
 	});
+
+	database.set(events);
 </script>
 
 <svelte:head>
